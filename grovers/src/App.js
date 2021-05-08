0e4d6689fd4c9953b7f9f5268a1840790e2f7c4b
abhishekanimatron <abhishekverma297ab@gmail.com>
2021-05-09 01:17:58 +0530

    grovers done

--- grovers/src/App.js
+++ grovers/src/App.js
@@ -2,32 +2,80 @@ import React, { useState, useEffect } from "react";
 import List from "./List";
 import Alert from "./Alert";
 
+// to get items from local storage
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    return JSON.parse(localStorage.getItem("list"));
+  } else {
+    return [];
+  }
+};
+
 function App() {
   const [name, setName] = useState("");
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStorage());
   const [isEditing, setIsEditing] = useState(false);
   const [editId, setEditId] = useState(null);
-  const [alertId, setAlertId] = useState({
-    show: true,
-    msg: "coool",
-    type: "success",
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: "",
+    type: "",
   });
+  // alert show help function
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, type, msg });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!name) {
+      showAlert(true, "danger", "Please enter value mmm");
     } else if (name && isEditing) {
+      setList(
+        list.map((item) => {
+          if (item.id === editId) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+      setName("");
+      setEditId(null);
+      setIsEditing(false);
+      showAlert(true, "success", "Content modified");
     } else {
+      showAlert(true, "success", "Item added successfully");
       const newItem = { id: new Date().getTime().toString(), title: name }; //unique id by time
       setList([...list, newItem]);
       setName("");
     }
   };
+  // delete all items
+  const clearList = () => {
+    showAlert(true, "danger", "List empty lol");
+    setList([]);
+  };
+  // delete specific item
+  const removeItem = (id) => {
+    showAlert(true, "danger", "Item erased");
+    setList(list.filter((item) => item.id !== id));
+  };
+  // to edit an item,
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditId(id);
+    setName(specificItem.title);
+  };
 
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]); //to persist data, we listen to list array and store in local storage
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
         <h3>grocery grovers</h3>
         <div className="form-control">
           <input
@@ -44,8 +92,10 @@ function App() {
       </form>
       {list.length > 0 && ( //show clear button only when there is something on list
         <div className="grocery-container">
-          <List items={list} />
-          <button className="clear-btn">Clear Items</button>
+          <List items={list} removeItem={removeItem} editItem={editItem} />
+          <button className="clear-btn" onClick={clearList}>
+            Clear Items
+          </button>
         </div>
       )}
     </section>
--- grovers/src/List.js
+++ grovers/src/List.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaEdit, FaTrash } from "react-icons/fa";
-const List = ({ items }) => {
+const List = ({ items, removeItem, editItem }) => {
   return (
     <div className="grocery-list">
       {items.map((item) => {
@@ -9,10 +9,18 @@ const List = ({ items }) => {
           <article className="grocery-item" key={id}>
             <p className="title">{title}</p>
             <div className="btn-container">
-              <button className="edit-btn" type="button">
+              <button
+                className="edit-btn"
+                type="button"
+                onClick={() => editItem(id)}
+              >
                 <FaEdit />
               </button>
-              <button className="delete-btn" type="button">
+              <button
+                className="delete-btn"
+                type="button"
+                onClick={() => removeItem(id)}
+              >
                 <FaTrash />
               </button>
             </div>
